7ee206507d490054df04f0680c025eea92b25cb8
Javier Lopez Franco <javilf@ad.unc.edu>
2021-09-22 20:54:45 -0400

    inutil

--- projects/cyoa.py
+++ projects/cyoa.py
@@ -7,6 +7,7 @@ from random import randint
 TREE: str = '\U0001F332'
 
 points: int = 100
+life_o: int = 100
 power_o: int = 15
 attack_o: str = "Z"
 
@@ -25,6 +26,10 @@ def main() -> None:
     print(creature_name + " is a great name!")
     legs: int = int(input("How many legs does " + creature_name + " have? "))
     arms: int = int(input("How many arms does " + creature_name + " have? "))
+    speed: int = 0
+    strenght: int = 0
+    life_o: int = 100
+    attack_o: str = "Z"
     opponent_name: str = input("What is your opponent's name? ")
 
     if legs < 2:
@@ -51,6 +56,8 @@ def main() -> None:
 
     power: int = (speed + strenght)
 
+    points: int = 0
+
     if power < 10:
         points: int = 130
     
@@ -62,8 +69,6 @@ def main() -> None:
 
     print(creature_name + " has an attack power of " + str(power) + " and a life of " + str(points))
 
-    life_o: int = 100
-    
     while points > 0:
         if int(life_o) > 0:
             print("What attack do you want to use?")
