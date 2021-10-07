6519ea14b2c0cd0256202455a6ec6584874235a3
Javier Lopez Franco <javilf@ad.unc.edu>
2021-10-07 00:29:34 -0400

    changes

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,5 +1,41 @@
 """List utility functions part 2."""
 
-__author__ = "123456789"
+__author__ = "730521799"
 
 # Define your functions below
+
+
+def only_evens(x: list) -> list:
+    """Return list with only evens."""
+    list_1: list = []
+    for item in x:
+        if (item % 2) == 0:
+            list_1.append(item)
+    return list_1
+
+
+def sub(x: list, y: int, z: int) -> list:
+    """Return list with only chosen values."""
+    list_2: list = []
+    for item in x:
+        if x[y] <= item <= x[z - 1]:
+            list_2.append(item)
+    return list_2
+
+
+def concat(x: list, y: list) -> list:
+    """Return list with only chosen values."""
+    list_3: list = []
+    for item in x:
+        list_3.append(item)
+    for item in y:
+        list_3.append(item)
+    return list_3
+
+
+print(only_evens([1, 2, 4]))
+
+a_list = [10, 20, 30, 40]
+print(sub(a_list, 1, 4))
+
+print(concat([1, 2, 3], [4, 7, 9]))
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -3,4 +3,63 @@
 # TODO: Uncomment the below line when ready to write unit tests
 # from exercises.ex05.utils import only_evens, sub, concat
 
-__author__ = "123456789"
+__author__ = "730521799"
+
+
+from ex05.utils import only_evens
+from ex05.utils import sub
+from ex05.utils import concat
+
+
+def test_only_evens_result() -> None:
+    x: list[int] = [1, 2, 3]
+    assert only_evens(x) == [2]
+
+
+def test_only_evens_result_b() -> None:
+    x: list[int] = [1, 2, 4]
+    assert only_evens(x) == [2, 4]
+
+
+def test_only_evens_result_c() -> None:
+    x: list[int] = [1, 3, 7]
+    assert only_evens(x) == []
+
+
+def test_sub_result() -> None:
+    x: list[int] = [10, 20, 30, 40]
+    y: int = 1
+    z: int = 4
+    assert sub(x, y, z) == [20, 30, 40]
+
+
+def test_sub_result_b() -> None:
+    x: list[int] = [10, 20, 30, 40]
+    y: int = 0
+    z: int = 2
+    assert sub(x, y, z) == [10, 20]
+
+
+def test_sub_result_c() -> None:
+    x: list[int] = [10, 20, 30, 40]
+    y: int = 1
+    z: int = 2
+    assert sub(x, y, z) == [20]
+
+
+def test_concat_result() -> None:
+    x: list[int] = [1, 2, 3]
+    y: list[int] = [4, 7, 10]
+    assert concat(x, y) == [1, 2, 3, 4, 7, 10]
+
+
+def test_concat_result_b() -> None:
+    x: list[int] = [1, 2, 3]
+    y: list[int] = [4, 6, 9]
+    assert concat(x, y) == [1, 2, 3, 4, 6, 9]
+
+
+def test_concat_result_c() -> None:
+    x: list[int] = [1, 2, 4]
+    y: list[int] = [5, 7, 9]
+    assert concat(x, y) == [1, 2, 4, 5, 7, 9]
