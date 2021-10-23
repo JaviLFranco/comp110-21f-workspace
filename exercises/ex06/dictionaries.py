8b0c2678d50cc33b0cdc2e1731a68d993fe47b56
Javier Lopez Franco <javilf@ad.unc.edu>
2021-10-23 19:37:25 -0400

    ex6

--- exercises/ex06/dictionaries.py
+++ exercises/ex06/dictionaries.py
@@ -1,5 +1,44 @@
 """Practice with dictionaries."""
 
-__author__ = "123456789"
+__author__ = "730521799"
 
 # Define your functions below
+
+
+# Function 1 Invert
+def invert(x: dict[str, str]) -> dict[str, str]:
+    """Invert Dictionary."""
+    dict_1: dict[str, str] = {}
+    dict_1 = {v: k for k, v in x.items()}
+    return dict_1
+
+
+# Function 2 Favorite colors
+def favorite_color(x: dict[str, str]) -> str:
+    """Find duplicates in Dictionary."""
+    dict_2: dict[str, int] = {}
+    for item in x:
+        if (x[item] in dict_2) is True:
+            dict_2[x[item]] += 1
+        else:
+            dict_2[x[item]] = 1
+    return max(dict_2, key=dict_2.get)
+
+
+# Function 3 Count
+def count(x: list[str]) -> dict[str, int]:
+    """Dictionary with values using another Dictionary."""
+    dict_3: dict[str, int] = {}
+    for item in x:
+        if (item in dict_3) is True:
+            dict_3[item] += 1
+        else:
+            dict_3[item] = 1
+    return(dict_3)
+
+
+print(invert({'a': 'z', 'b': 'y', 'c': 'x'}))
+
+print(favorite_color({"Marc": "yellow", "Ezri": "blue", "Kris": "blue", "Mark": "yellow"}))
+
+print(count(["blue", "yellow", "blue", "blue", "green"]))
--- exercises/ex06/dictionaries_test.py
+++ exercises/ex06/dictionaries_test.py
@@ -3,4 +3,71 @@
 # TODO: Uncomment the below line when ready to write unit tests
 # from exercises.ex06.dictionaries import invert, favorite_color, count
 
-__author__ = "123456789"
+__author__ = "730521799"
+
+from ex06.dictionaries import invert
+from ex06.dictionaries import favorite_color
+from ex06.dictionaries import count
+
+
+# Test 1 for invert
+def test_invert_result_a() -> None:
+    """Return inverted value 1."""
+    x: dict[str, str] = {'a': 'z', 'b': 'y', 'c': 'x'}
+    assert "z" in invert(x)
+
+
+# Test 2 for invert
+def test_invert_result_b() -> None:
+    """Return inverted value 2."""
+    x: dict[str, str] = {'a': 'z', 'b': 'y', 'c': 'x'}
+    assert "y" in invert(x)
+
+
+# Test 3 for invert
+def test_invert_result_c() -> None:
+    """Return inverted values."""
+    x: dict[str, str] = {'a': 'z', 'b': 'y', 'c': 'x'}
+    assert invert(x) == {'z': 'a', 'y': 'b', 'x': 'c'}
+
+
+# Test 1 for favorite_color
+def test_favorite_color_result_a() -> None:
+    """Return max value 1."""
+    x: dict[str, str] = {"Marc": "green", "Ezri": "blue", "Kris": "green", "Mark": "green"}
+    assert favorite_color(x) == "green"
+
+
+# Test 2 for favorite_color
+def test_favorite_color_result_b() -> None:
+    """Return max value 2."""
+    x: dict[str, str] = {"Marc": "green", "Ezri": "blue", "Kris": "blue", "Mark": "yellow"}
+    assert favorite_color(x) == "blue"
+
+
+# Test 3 for favorite_color
+def test_favorite_color_result_c() -> None:
+    """Return max value 3."""
+    x: dict[str, str] = {"Marc": "yellow", "Ezri": "blue", "Kris": "blue", "Mark": "yellow"}
+    assert favorite_color(x) == "yellow"
+
+
+# Test 1 for count
+def test_count_result_a() -> None:
+    """Return inverted value 1."""
+    x: list[str] = ["blue", "yellow", "blue", "blue", "green"]
+    assert "green" in count(x)
+
+
+# Test 2 for count
+def test_count_result_b() -> None:
+    """Return inverted value 2."""
+    x: list[str] = ["blue", "yellow", "blue", "blue", "green"]
+    assert "blue" in count(x)
+
+
+# Test 3 for count
+def test_count_result_c() -> None:
+    """Return inverted values."""
+    x: list[str] = ["blue", "yellow", "blue", "blue", "green"]
+    assert count(x) == {'blue': 3, 'yellow': 1, 'green': 1}
