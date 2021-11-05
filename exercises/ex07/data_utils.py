7743c5e4cfcc3c881a8fe26e2fc64c3c46ecf100
Javier Lopez Franco <javilf@ad.unc.edu>
2021-11-05 18:20:30 -0400

    ex7

--- exercises/ex07/data_utils.py
+++ exercises/ex07/data_utils.py
@@ -1,5 +1,137 @@
 """Utility functions."""
 
-__author__ = "123456789"
+__author__ = "730521799"
 
 # Define your functions below
+
+
+from csv import DictReader
+
+
+# Read CSV Files
+def read_csv_rows(filename: str) -> list[dict[str, str]]:
+    """Function that reads csv files in a list."""
+    result: list[dict[str, str]] = []
+
+    # Open File
+    file_handle = open(filename, "r", encoding="utf8")
+
+    # Open a handle
+    csv_reader = DictReader(file_handle)
+
+    # Read each row
+    for row in csv_reader:
+        result.append(row)
+
+    # Close file
+    file_handle.close()
+
+    return result
+
+
+# Creates a list
+def column_values(x: list[dict[str, str]], y: str) -> list[str]:
+    """Function that creates a list with column values."""
+
+    # Empty List
+    list_1: list[str] = []
+
+    # Loop
+    for item in x:
+        if y in item:
+            list_1.append(item[y])
+
+    # Return
+    return list_1
+
+
+# Transform list in a table with columns
+def columnar(x: list[dict[str, str]]) -> dict[str, list[str]]:
+    """List to columns."""
+
+    # Dict value
+    result: dict[str, list[str]] = {}
+
+    # Row value
+    first_row: dict[str, str] = x[0]
+    for column in first_row:
+        result[column] = column_values(x, column)
+    
+    return result
+
+
+def head(x: dict[str, list[str]], y: int) -> dict[str, list[str]]:
+    """Number of columns only."""
+
+    # Dict value
+    result: dict[str, list[str]] = {}
+
+    dict_1: dict[str, list[str]] = x
+
+    # Counter value
+    i: int = 0
+    while i >= (y - 1):
+        iterator = iter(dict_1)
+        first = next(iterator)
+        result[first] = dict_1[first]
+        del dict_1[first]
+        i = i + 1
+    
+    return result
+
+
+def select(x: dict[str, list[str]], y: list[str]) -> dict[str, list[str]]:
+    """Select columns"""
+    result: dict[str, list[str]] = {}
+    i = 2
+
+    for column in x:
+        if y[0] in column:
+            if y[0] in result:
+                i = 1
+            else:
+                result[y[0]] = x[y[0]]
+        if y[1] in column:
+            if y[1] in result:
+                if i == 0:
+                    print(i)
+            else:
+                result[y[1]] = x[y[0]]
+    
+    return result
+        
+
+def concat(x: dict[str, list[str]], y: dict[str, list[str]]) -> dict[str, list[str]]:
+    """Add values to dict."""
+    result: dict[str, list[str]] = {}
+
+    # For Dict x
+    for item in x:
+        result[item] = x[item]
+    
+    # For Dict y
+    for item in y:
+        if item in result:
+            for item_b in y[item]:
+                result[item].append(item_b)
+        
+        else:
+            result[item] = y[item]
+
+    return result
+
+
+def count(x: list[str]) -> dict[str, int]:
+    """Find number of times in a dict."""
+    result: dict[str, int] = {}
+
+    # Add Values
+    for item in x:
+
+        if item in result:
+            result[item] += 1
+        else:
+            result[item] = 0
+            result[item] += 1
+    
+    return result
--- lessons/input.py
+++ lessons/input.py
@@ -2,3 +2,33 @@
 
 print("Wow, " + input("Hey, who are you? ") + ", you rock!")
 print(input("Who are you? ") + ", have a great day!")
+
+
+def head(x: dict[str, list[str]], y: int) -> dict[str, list[str]]:
+    "Number of columns only."
+
+    # Dict value
+    result: dict[str, list[str]] = {}
+
+    list_1: list[str] = []
+
+    for column in x:
+        # Counter value
+        i: int = 0
+        while i < y and i < len(x[column]):
+            list_1.append(x[column][i])
+            i = i + 1
+    
+    return result
+
+
+
+def select(x: dict[str, list[str]], y: list[str]) -> dict[str, list[str]]:
+    """Select columns"""
+    result: dict[str, list[str]] = {}
+
+    # Add item
+    for item in y:
+        result[item] = x[item]
+    
+    return result
